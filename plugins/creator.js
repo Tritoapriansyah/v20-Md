9cab04ae41bea98fce0ee2faff1d03d16c7b3a50
trito apriansyah <48681738+Tritoapriansyah@users.noreply.github.com>
2022-02-23 07:25:47 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,10 +2,10 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;KingOfBear;;;'
-                    + 'FN:KingOfBear\n' // full name
-                    + 'ORG:Owner YukiBot;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6288279268363:+6288279268363\n' // WhatsApp ID + phone number
+                    + 'N:;Trito;;;'
+                    + 'FN:Trito\n' // full name
+                    + 'ORG:Owner LolitaBot;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6285888258313:+6285888258313\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'King Of Bear', contacts: [{ vcard }] } }, { quoted: m })
 }
